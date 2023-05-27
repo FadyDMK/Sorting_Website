e2551568778628ea9f083ff79a7e29090cc1323f
Fady DMK <32818953+FadyDMK@users.noreply.github.com>
2023-05-27 17:13:58 +0200

    finished bubble sorting

--- js/main.js
+++ js/main.js
@@ -14,21 +14,41 @@
   }
 })();
 
-function start()  {
-    var elements = document.getElementsByClassName('rectangle');
-    var i=1,j,t,p;
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-    for(i=0;i<elements.length;i++){
-        for (j = 0; j <= elements.length-1; j++){
+async function start()  {
+    var elements = document.getElementsByClassName('rectangle');
+    var i,j,t,p;
+    var delayInMilliseconds = 10000; //1 second
+
+
+
+    for(i=0;i<elements.length-1;i++){
+        
+        for (j = 0; j < elements.length-1-i; j++){
+
+            
+            elements[j].style.backgroundColor = "red";
+            elements[j+1].style.backgroundColor  = "red";
+
             t=parseInt(window.getComputedStyle(elements[j]).height, 10);
             p=parseInt(window.getComputedStyle(elements[j+1]).height, 10);
+            
             if (t>p){
-                
                 elements[j].style.height = p + 'px';
                 elements[j+1].style.height = t + 'px';
             }
+
+            
+
+            
+            await delay(100); // Adjust the delay duration as needed (in milliseconds)
+            elements[j].style.backgroundColor = "blueviolet";
+            elements[j+1].style.backgroundColor  = "blueviolet";
+
         }
     }
-    }
     
-
+}
