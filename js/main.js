3ace9376cad8cd144788c6958f93479641ddd9c3
Fady DMK <32818953+FadyDMK@users.noreply.github.com>
2023-05-27 16:39:25 +0200

    added bubble sort

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//this function randomizes the heights of the bars and organises them next to each other
 (function () {
     var elements = document.getElementsByClassName('rectangle');
     var i,r,marginLeft=-10.5;
@@ -6,9 +7,28 @@
     for (i = 0; i < elements.length; i++) {
     marginLeft += parseInt(window.getComputedStyle(elements[i]).marginLeft, 10) + 11;
     elements[i].style.marginLeft = marginLeft + '%';
-    r=Math.floor(Math.random() * 70) + 1;
-    elements[i].style.height = r + '%';
+    r=Math.floor(Math.random() * 500) + 1;
+    elements[i].style.height = r + "px";
 
 
   }
 })();
+
+function start()  {
+    var elements = document.getElementsByClassName('rectangle');
+    var i=1,j,t,p;
+
+    for(i=0;i<elements.length;i++){
+        for (j = 0; j <= elements.length-1; j++){
+            t=parseInt(window.getComputedStyle(elements[j]).height, 10);
+            p=parseInt(window.getComputedStyle(elements[j+1]).height, 10);
+            if (t>p){
+                
+                elements[j].style.height = p + 'px';
+                elements[j+1].style.height = t + 'px';
+            }
+        }
+    }
+    }
+    
+
